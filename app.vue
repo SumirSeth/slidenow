570219239c71cc3b8454fb98bc97bcf4f17cb664
Sumir Seth <iamsumirseth@gmail.com>
2025-02-16 16:39:10 +0530

    chore: add title and meta tags for SEO optimization

--- app.vue
+++ app.vue
@@ -1,4 +1,8 @@
 <template>
+  <Title>SlideNow</Title>
+  <Link rel="icon" href="/favicon.png" />
+  <Meta name="description" content="SlideNow" />
+  <Meta name="keywords" content="SlideNow" />
   <div>
     <NuxtPage />
   </div>
